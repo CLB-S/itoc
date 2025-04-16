ab6d06e73b2ccc614a11cb4b18ac44b2145c1d56
LeSnow Ye <lesnow.ye@gmail.com>
2025-04-17 00:02:52 +0800

    feat: Normalized world settings.

--- scripts/world/WorldSettings.cs
+++ scripts/world/WorldSettings.cs
@@ -3,19 +3,23 @@ using System;
 
 public class WorldSettings
 {
+    private float _minimumCellDistance = 1.0f;
+    private float _noiseFrequency = 1.0f;
+
     public ulong Seed = 234;
 
     #region Generation Settings
-    public float ContinentRatio = 0.5f;
+    public float ContinentRatio = 0.6f;
     public float PlateMergeRatio = 0.13f;
     public float MaxTectonicMovement = 10.0f;
     public float MaxAltitude = 1000.0f;
 
-    public Rect2I Bounds = new Rect2I(-10000, -10000, 20000, 20000);
-    public float MinimumCellDistance = 100;
+    public Rect2I Bounds = new Rect2I(-20000, -20000, 40000, 40000);
+    public float MinimumCellDistance { get => _minimumCellDistance * Bounds.Size.Y / 200.0f; set => _minimumCellDistance = value; }
+    public float NoiseFrequency { get => _noiseFrequency * Bounds.Size.Y / 4000.0f; set => _noiseFrequency = value; }
+
     public float AltitudePropagationDecrement = 0.8f;
     public float AltitudePropagationSharpness = 0.1f;
-    public float NoiseFrequency = 5.0f;
     #endregion
 
     // Fluvial erosion settings
