9a510328c080767282a436a52c870f89f2ebf3be
LeSnow-Ye <lesnow.ye@gmail.com>
2025-04-17 11:34:15 +0800

    fix: Redundant opaque block meshing.

--- scripts/chunk/ChunkMesher.cs
+++ scripts/chunk/ChunkMesher.cs
@@ -65,9 +65,10 @@ public static class ChunkMesher
 
                 if (meshData.WaterMasks != null)
                 {
-                    columnBits = (meshData.WaterMasks[a * CS_P + b] | meshData.OpaqueMask[a * CS_P + b]) & ~((1UL << 63) | 1);
+                    // Water top face.
+                    meshData.FaceMasks[baIndex + 0 * CS_2] |= (meshData.WaterMasks[a * CS_P + b] & ~((1UL << 63) | 1) & (~meshData.WaterMasks[aCS_P + CS_P + b])) >> 1; // +Y
 
-                    meshData.FaceMasks[baIndex + 0 * CS_2] |= (columnBits & (~meshData.WaterMasks[aCS_P + CS_P + b])) >> 1; // +Y
+                    columnBits = (meshData.WaterMasks[a * CS_P + b] | meshData.OpaqueMask[a * CS_P + b]) & ~((1UL << 63) | 1);
                     meshData.FaceMasks[baIndex + 1 * CS_2] |= (columnBits & (~(meshData.WaterMasks[aCS_P - CS_P + b] | meshData.OpaqueMask[aCS_P - CS_P + b]))) >> 1;
                     meshData.FaceMasks[abIndex + 2 * CS_2] |= (columnBits & (~(meshData.WaterMasks[aCS_P + b + 1] | meshData.OpaqueMask[aCS_P + b + 1]))) >> 1;
                     meshData.FaceMasks[abIndex + 3 * CS_2] |= (columnBits & (~(meshData.WaterMasks[aCS_P + (b - 1)] | meshData.OpaqueMask[aCS_P + (b - 1)]))) >> 1;
