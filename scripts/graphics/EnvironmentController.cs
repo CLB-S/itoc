551d88a49414a5a5a561d4548553f994bfc8da23
LeSnow-Ye <lesnow.ye@gmail.com>
2025-04-18 11:11:17 +0800

    graphics: Exponential fog & SSAO

--- scripts/graphics/EnvironmentController.cs
+++ scripts/graphics/EnvironmentController.cs
@@ -4,8 +4,8 @@ public partial class EnvironmentController : WorldEnvironment
 {
     public override void _Ready()
     {
-        float viewDistance = Core.Instance.Settings.RenderDistance * World.ChunkSize;
-        Environment.FogDepthBegin = viewDistance * 0.85f;
-        Environment.FogDepthEnd = viewDistance * 0.95f;
+        // float viewDistance = Core.Instance.Settings.RenderDistance * World.ChunkSize;
+        // Environment.FogDepthBegin = viewDistance * 0.85f;
+        // Environment.FogDepthEnd = viewDistance * 0.95f;
     }
 }
