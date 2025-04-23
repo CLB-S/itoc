f3e710dc301b8a0ce132463ee0931acc69fe7838
LeSnow-Ye <lesnow.ye@gmail.com>
2025-04-23 20:20:09 +0800

    feat: Keep` _settings`for serialization.

--- scripts/libs/pattern_system/generator_nodes/FastNoiseLightNode.cs
+++ scripts/libs/pattern_system/generator_nodes/FastNoiseLightNode.cs
@@ -7,33 +7,60 @@ namespace PatternSystem;
 public class FastNoiseLiteNode : PatternTreeNode
 {
     private FastNoiseLite _noiseGenerator;
+    private FastNoiseLiteSettings _settings;
+
     public FastNoiseLiteNode(FastNoiseLite fastNoiseLite)
     {
         _noiseGenerator = fastNoiseLite;
+        _settings = new FastNoiseLiteSettings
+        {
+            CellularDistanceFunction = (CellularDistanceFunction)_noiseGenerator.CellularDistanceFunction,
+            CellularJitter = _noiseGenerator.CellularJitter,
+            CellularReturnType = (CellularReturnType)_noiseGenerator.CellularReturnType,
+            DomainWarpAmplitude = _noiseGenerator.DomainWarpAmplitude,
+            DomainWarpEnabled = _noiseGenerator.DomainWarpEnabled,
+            DomainWarpFractalGain = _noiseGenerator.DomainWarpFractalGain,
+            DomainWarpFractalLacunarity = _noiseGenerator.DomainWarpFractalLacunarity,
+            DomainWarpFractalOctaves = _noiseGenerator.DomainWarpFractalOctaves,
+            DomainWarpFractalType = (DomainWarpFractalType)_noiseGenerator.DomainWarpFractalType,
+            DomainWarpFrequency = _noiseGenerator.DomainWarpFrequency,
+            DomainWarpType = (DomainWarpType)_noiseGenerator.DomainWarpType,
+            FractalGain = _noiseGenerator.FractalGain,
+            FractalLacunarity = _noiseGenerator.FractalLacunarity,
+            FractalOctaves = _noiseGenerator.FractalOctaves,
+            FractalPingPongStrength = _noiseGenerator.FractalPingPongStrength,
+            FractalType = (FractalType)_noiseGenerator.FractalType,
+            FractalWeightedStrength = _noiseGenerator.FractalWeightedStrength,
+            Frequency = _noiseGenerator.Frequency,
+            NoiseType = (NoiseType)_noiseGenerator.NoiseType,
+            Offset = _noiseGenerator.Offset,
+            Seed = _noiseGenerator.Seed
+        };
     }
 
     public FastNoiseLiteNode(FastNoiseLiteSettings settings)
     {
+        _settings = settings;
         _noiseGenerator = new FastNoiseLite();
-        _noiseGenerator.SetCellularDistanceFunction((FastNoiseLite.CellularDistanceFunctionEnum)(int)settings.CellularDistanceFunction);
+        _noiseGenerator.SetCellularDistanceFunction((FastNoiseLite.CellularDistanceFunctionEnum)settings.CellularDistanceFunction);
         _noiseGenerator.SetCellularJitter(settings.CellularJitter);
-        _noiseGenerator.SetCellularReturnType((FastNoiseLite.CellularReturnTypeEnum)(int)settings.CellularReturnType);
+        _noiseGenerator.SetCellularReturnType((FastNoiseLite.CellularReturnTypeEnum)settings.CellularReturnType);
         _noiseGenerator.SetDomainWarpAmplitude(settings.DomainWarpAmplitude);
         _noiseGenerator.SetDomainWarpEnabled(settings.DomainWarpEnabled);
         _noiseGenerator.SetDomainWarpFractalGain(settings.DomainWarpFractalGain);
         _noiseGenerator.SetDomainWarpFractalLacunarity(settings.DomainWarpFractalLacunarity);
         _noiseGenerator.SetDomainWarpFractalOctaves(settings.DomainWarpFractalOctaves);
-        _noiseGenerator.SetDomainWarpFractalType((FastNoiseLite.DomainWarpFractalTypeEnum)(int)settings.DomainWarpFractalType);
+        _noiseGenerator.SetDomainWarpFractalType((FastNoiseLite.DomainWarpFractalTypeEnum)settings.DomainWarpFractalType);
         _noiseGenerator.SetDomainWarpFrequency(settings.DomainWarpFrequency);
-        _noiseGenerator.SetDomainWarpType((FastNoiseLite.DomainWarpTypeEnum)(int)settings.DomainWarpType);
+        _noiseGenerator.SetDomainWarpType((FastNoiseLite.DomainWarpTypeEnum)settings.DomainWarpType);
         _noiseGenerator.SetFractalGain(settings.FractalGain);
         _noiseGenerator.SetFractalLacunarity(settings.FractalLacunarity);
         _noiseGenerator.SetFractalOctaves(settings.FractalOctaves);
         _noiseGenerator.SetFractalPingPongStrength(settings.FractalPingPongStrength);
-        _noiseGenerator.SetFractalType((FastNoiseLite.FractalTypeEnum)(int)settings.FractalType);
+        _noiseGenerator.SetFractalType((FastNoiseLite.FractalTypeEnum)settings.FractalType);
         _noiseGenerator.SetFractalWeightedStrength(settings.FractalWeightedStrength);
         _noiseGenerator.SetFrequency(settings.Frequency);
-        _noiseGenerator.SetNoiseType((FastNoiseLite.NoiseTypeEnum)(int)settings.NoiseType);
+        _noiseGenerator.SetNoiseType((FastNoiseLite.NoiseTypeEnum)settings.NoiseType);
         _noiseGenerator.SetOffset(settings.Offset);
         _noiseGenerator.SetSeed(settings.Seed);
     }
