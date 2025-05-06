cffa0c61c05aa24d11a93376caa0ee76b853c34f
LeSnow-Ye <lesnow.ye@gmail.com>
2025-05-06 11:58:44 +0800

    docs: Document `WorldGeneratorUtils`.

--- scripts/world/generator/WorldGeneratorUtils.cs
+++ scripts/world/generator/WorldGeneratorUtils.cs
@@ -8,6 +8,13 @@ namespace WorldGenerator;
 
 public partial class WorldGenerator
 {
+    /// <summary>
+    /// Creates duplicate points around the edges of a region to ensure continuity in a wrapped world.
+    /// </summary>
+    /// <param name="points">The original list of points</param>
+    /// <param name="rect">The rectangular bounds of the region</param>
+    /// <param name="edgeDistance">The distance from the edge within which points should be repeated</param>
+    /// <returns>A dictionary mapping duplicated point indices to their original indices</returns>
     private static Dictionary<int, int> RepeatPointsRoundEdges(List<Vector2> points, Rect2 rect, double edgeDistance)
     {
         var indexMap = new Dictionary<int, int>();
@@ -69,6 +76,11 @@ public partial class WorldGenerator
         return indexMap;
     }
 
+    /// <summary>
+    /// Transforms a position to ensure it lies within the world's bounds.
+    /// </summary>
+    /// <param name="position">The position to normalize</param>
+    /// <returns>The position wrapped within the world bounds</returns>
     public Vector2 UniformPosition(Vector2 position)
     {
         position -= Settings.Bounds.Position;
@@ -78,6 +90,10 @@ public partial class WorldGenerator
         ) + Settings.Bounds.Position;
     }
 
+    /// <summary>
+    /// Calculates the shortest distance between two points in a wrapped world.
+    /// </summary>
+    /// <returns>The distance between the two points, accounting for world wrapping</returns>
     public double UniformDistance(Vector2 pos1, Vector2 pos2)
     {
         var dx = Mathf.Abs(pos1.X - pos2.X);
@@ -92,11 +108,21 @@ public partial class WorldGenerator
         return Mathf.Sqrt(dx * dx + dy * dy);
     }
 
+    /// <summary>
+    /// Gets the indices of cells that are neighbors to the specified cell.
+    /// </summary>
+    /// <param name="cell">The cell to find neighbors for</param>
+    /// <returns>An enumerable of neighbor cell indices</returns>
     public IEnumerable<int> GetNeighborCellIndices(CellData cell)
     {
         return GetNeighborCellIndices(cell.Index);
     }
 
+    /// <summary>
+    /// Gets the indices of cells that are neighbors to the cell with the specified index.
+    /// </summary>
+    /// <param name="index">The index of the cell to find neighbors for</param>
+    /// <returns>An enumerable of neighbor cell indices</returns>
     public IEnumerable<int> GetNeighborCellIndices(int index)
     {
         foreach (var i in _delaunator.EdgesAroundPoint(Delaunator.PreviousHalfedge(_triangleIndicesMap[index])))
@@ -110,18 +136,33 @@ public partial class WorldGenerator
         }
     }
 
+    /// <summary>
+    /// Gets the cells that are neighbors to the specified cell.
+    /// </summary>
+    /// <param name="cell">The cell to find neighbors for</param>
+    /// <returns>An enumerable of neighbor cells</returns>
     public IEnumerable<CellData> GetNeighborCells(CellData cell)
     {
         foreach (var i in GetNeighborCellIndices(cell))
             yield return _cellDatas[i];
     }
 
+    /// <summary>
+    /// Gets the cells that are neighbors to the cell with the specified index.
+    /// </summary>
+    /// <param name="index">The index of the cell to find neighbors for</param>
+    /// <returns>An enumerable of neighbor cells</returns>
     public IEnumerable<CellData> GetNeighborCells(int index)
     {
         foreach (var i in GetNeighborCellIndices(index))
             yield return _cellDatas[i];
     }
 
+    /// <summary>
+    /// Converts a position to a latitude value in degrees.
+    /// </summary>
+    /// <param name="position">The position to convert</param>
+    /// <returns>The latitude in degrees, ranging from -90 to 90</returns>
     public double GetLatitude(Vector2 position)
     {
         var normalizedPos = (position - Settings.WorldCenter) / Settings.Bounds.Size +
@@ -129,6 +170,11 @@ public partial class WorldGenerator
         return -Mathf.Lerp(-90, 90, normalizedPos.Y);
     }
 
+    /// <summary>
+    /// Converts a position to a longitude value in degrees.
+    /// </summary>
+    /// <param name="position">The position to convert</param>
+    /// <returns>The longitude in degrees, ranging from -180 to 180</returns>
     public double GetLongitude(Vector2 position)
     {
         var normalizedPos = (position - Settings.WorldCenter) / Settings.Bounds.Size +
@@ -136,6 +182,13 @@ public partial class WorldGenerator
         return Mathf.Lerp(-180, 180, normalizedPos.X);
     }
 
+    /// <summary>
+    /// Finds cell data near a specified position.
+    /// </summary>
+    /// <param name="x">The x-coordinate</param>
+    /// <param name="y">The y-coordinate</param>
+    /// <param name="numNeighbors">The number of nearby cells to find</param>
+    /// <returns>An enumerable of the nearest cell data</returns>
     public IEnumerable<CellData> FindCellDatasNearby(double x, double y, int numNeighbors = 1)
     {
         if (State != GenerationState.Completed)
@@ -152,11 +205,23 @@ public partial class WorldGenerator
             yield return _cellDatas[data.Item2];
     }
 
+    /// <summary>
+    /// Gets cell data near a specified position.
+    /// </summary>
+    /// <param name="pos">The position to search near</param>
+    /// <param name="numNeighbors">The number of nearby cells to find</param>
+    /// <returns>An enumerable of the nearest cell data</returns>
     public IEnumerable<CellData> GetCellDatasNearby(Vector2 pos, int numNeighbors = 1)
     {
         return FindCellDatasNearby(pos.X, pos.Y, numNeighbors);
     }
 
+    /// <summary>
+    /// Finds the triangle that contains a specified point and calculates its barycentric coordinates.
+    /// </summary>
+    /// <param name="point">The point to locate</param>
+    /// <param name="barycentricPos">The barycentric coordinates of the point within the triangle</param>
+    /// <returns>A tuple of the three vertex indices that form the triangle</returns>
     public (int, int, int) GetTriangleContainingPoint(Vector2 point, out Vector3 barycentricPos)
     {
         var mappedX = 2 * Mathf.Pi * point.X / Settings.Bounds.Size.X;
@@ -181,16 +246,34 @@ public partial class WorldGenerator
         throw new InvalidOperationException("No triangle found containing the point.");
     }
 
+    /// <summary>
+    /// Finds the triangle that contains a specified point.
+    /// </summary>
+    /// <param name="point">The point to locate</param>
+    /// <returns>A tuple of the three vertex indices that form the triangle</returns>
     public (int, int, int) GetTriangleContainingPoint(Vector2 point)
     {
         return GetTriangleContainingPoint(point, out _);
     }
 
+    /// <summary>
+    /// Finds the triangle that contains a specified point and calculates its barycentric coordinates.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the point</param>
+    /// <param name="y">The y-coordinate of the point</param>
+    /// <param name="barycentricPos">The barycentric coordinates of the point within the triangle</param>
+    /// <returns>A tuple of the three vertex indices that form the triangle</returns>
     public (int, int, int) GetTriangleContainingPoint(double x, double y, out Vector3 barycentricPos)
     {
         return GetTriangleContainingPoint(new Vector2(x, y), out barycentricPos);
     }
 
+    /// <summary>
+    /// Finds the triangle that contains a specified point.
+    /// </summary>
+    /// <param name="x">The x-coordinate of the point</param>
+    /// <param name="y">The y-coordinate of the point</returns>
+    /// <returns>A tuple of the three vertex indices that form the triangle</returns>
     public (int, int, int) GetTriangleContainingPoint(double x, double y)
     {
         return GetTriangleContainingPoint(new Vector2(x, y), out _);
